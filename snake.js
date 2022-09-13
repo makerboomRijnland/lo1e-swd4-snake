00e87fe81d52b10f6f4a1cc0504406aa2c68f2c1
makerboomRijnland <makerboom@mborijnland.nl>
2022-09-13 12:58:03 +0200

    Drawing the snake

--- snake.js
+++ snake.js
@@ -1,5 +1,10 @@
 let canvas = document.getElementById('snake');
 let context = canvas.getContext('2d');
 
+// Draw the background
 context.fillStyle = "black";
 context.fillRect(0, 0, 400, 400);
+
+// Draw the snake
+context.fillStyle = 'white';
+context.fillRect(200, 200, 20, 20);
