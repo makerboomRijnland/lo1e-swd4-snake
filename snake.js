11784e3350052d87cb08567efa22144a95e95263
makerboomRijnland <makerboom@mborijnland.nl>
2022-09-13 13:20:58 +0200

    interval game update

--- snake.js
+++ snake.js
@@ -8,3 +8,11 @@ context.fillRect(0, 0, 400, 400);
 // Draw the snake
 context.fillStyle = 'white';
 context.fillRect(200, 200, 20, 20);
+
+
+function update() {
+    console.log('update');
+}
+
+
+setInterval(update, 1000);
