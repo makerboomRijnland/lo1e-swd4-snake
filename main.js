2783542a835bccb95a9d54bc64ff4a451f853311
Nielskampioen2 <6017703@mborijnland.nl>
2022-09-13 13:21:18 +0200

    Interval update

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-const canvas = document.getElementById('snake');
+const canvas = document.getElementById("snake");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, 400, 400);
+
+ctx.fillStyle = "green";
+ctx.fillRect(200, 200, 20, 20);
+
+function update(){
+    console.log('update')
+}
+
+setInterval(update, 0);
