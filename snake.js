1baaaf917ebf32a8af0166f92323128b2a10869f
makerboomRijnland <makerboom@mborijnland.nl>
2022-09-20 13:11:14 +0200

    Richting veranderen met toetsen

--- snake.js
+++ snake.js
@@ -1,10 +1,10 @@
-let canvas = document.getElementById('snake');
-let context = canvas.getContext('2d');
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
 
 let snakeX = 200;
 let snakeY = 200;
 
-let direction = 'up';
+let direction = null;
 
 // Draw the background
 function drawBackground() {
@@ -14,19 +14,18 @@ function drawBackground() {
 
 // Draw the snake
 function drawSnake() {
-    context.fillStyle = 'white';
+    context.fillStyle = "white";
     context.fillRect(snakeX, snakeY, 20, 20);
 }
 
-
 function update() {
-    if(direction == 'right') {
+    if (direction == "right") {
         snakeX += 20;
-    } else if(direction == 'left') {
+    } else if (direction == "left") {
         snakeX -= 20;
-    } else if(direction == 'up') { 
+    } else if (direction == "up") {
         snakeY -= 20;
-    } else if(direction == 'down') { 
+    } else if (direction == "down") {
         snakeY += 20;
     }
 
@@ -34,6 +33,19 @@ function update() {
     drawSnake();
 }
 
+function changeDirection(event) {
+    if (event.code == "ArrowUp") {
+        direction = "up";
+    } else if (event.code == "ArrowRight") {
+        direction = "right";
+    } else if (event.code == "ArrowLeft") {
+        direction = "left";
+    } else if (event.code == "ArrowDown") {
+        direction = "down";
+    }
+}
+
 drawBackground();
 drawSnake();
 setInterval(update, 1000);
+addEventListener("keydown", changeDirection);
